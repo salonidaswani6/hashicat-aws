0b39631ef5ac75149050eeb2bba3c26d335419f4
salonidaswani6 <87329775+salonidaswani6@users.noreply.github.com>
2021-07-12 14:33:59 -0400

    Create s3-bucket.tf

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
+  source  = "app.terraform.io/SALONI-training/s3-bucket/aws"
+  version = "2.6.0"
+
+  acceleration_status = "Suspended"
+  bucket = "test"
+  bucket_prefix = "test-terraform"
+  policy = {   statement {     sid = "1"      actions = [       "s3:ListAllMyBuckets",       "s3:GetBucketLocation",     ]      resources = [       "arn:aws:s3:::*",     ]   }
+  request_payer = "BucketOwner"
+}
